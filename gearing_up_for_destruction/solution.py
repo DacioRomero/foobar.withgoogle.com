9500acbc98545756403ef4c9dfd23b6135db59dc
Dacio Romero <dacioromero@gmail.com>
2018-09-18 13:28:20 -0700

    Add progress for "gearing_up_for_destruction"

--- gearing_up_for_destruction/solution.py
+++ gearing_up_for_destruction/solution.py
@@ -7,31 +7,37 @@ def answer(pegs):
 
 
 def answer_distances(distances):
-    distances_len = len(distances)
+    dists_len = len(distances)
+    gears = []
 
-    raw_result = 0
+    dists_flipflop = [-e if (dists_len % 2 == 0) == (i % 2 == 0) else e for i, e in enumerate(distances)]
+    gears.append(sum(dists_flipflop) / (-0.5 if dists_flipflop[0] < 0 else 1.5))
 
-    for i in range(distances_len):
-        if (distances_len % 2 == 0) == (i % 2 == 0):
-            raw_result -= distances[i]
-        else:
-            raw_result += distances[i]
+    print('gears[0]', gears[0])
 
-    raw_result /= -0.5 if distances_len % 2 == 0 else 1.5
+    # if gears[0] < 1:  # or gears[0] > distances[0]:
+    #     return [-1, -1]
 
-    if raw_result >= 1 and raw_result < distances[0]:
-        fraction = Fraction(raw_result)
-        return [fraction.numerator, fraction.denominator]
+    for i in range(1, dists_len + 1):
+        slice_dist_flipflop = dists_flipflop[:i]
+        if i % 2 != 0:
+            slice_dist_flipflop = [-e for e in slice_dist_flipflop]
 
-    return [-1, -1]
+        # print('slice_dist_flipflop', slice_dist_flipflop)
+        gears.append(sum(slice_dist_flipflop) + (gears[0] if slice_dist_flipflop[0] < 0 else -gears[0]))
+        # print('gears[{}] {}'.format(i, gears[i]))
 
+        if gears[i] < 0 or (gears[i] + gears[i - 1]) != distances[i - 1]:
+            return [-1, -1]
 
-def verify_gears(gear, distances):
-    pass
+    print('gears', gears)
+    fraction = Fraction(gears[0])
+    return [fraction.numerator, fraction.denominator]
 
 
 if __name__ == '__main__':
     print(answer([4, 30, 50]))
+    print('-' * 32)
     print(answer([4, 17, 50]))
-
+    print('-' * 32)
     print(answer_distances([35, 3, 16, 28, 24, 7]))
